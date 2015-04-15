cdbb7dadb7793b569a5795e9ce636b75c34fddca
Justin <justingoldsmith@my.tridenttech.edu>
2015-04-15 13:02:31 -0400

    testing a few things

--- index/index-scripts.js
+++ index/index-scripts.js
@@ -13,14 +13,27 @@ var underAge = function() {
 
 
 function validate_age() {
-	var age_year = parseInt(document.getElementById("year").value);
+	//var age_year = parseInt(document.getElementById("year").value);
+	//birthDate();
 
-	if (this_year - age_year >= 18) {
-		legalAge();
-	} else {
-		alert("Sorry, we can't let you in if you're under 18. Thanks for understanding!");
-		underAge();
-	}
+	//if (this_year - age_year >= 18) {
+		//legalAge();
+		alert(birthDate());
+	//} else {
+		//alert("Sorry, we can't let you in if you're under 18. Thanks for understanding!");
+		//underAge();
+		//alert(date);
+	//}
+}
+
+var birthDate = function() {
+	var mm = document.getElementById('month').value;
+	var dd = document.getElementById('day').value;
+	var yyyy = document.getElementById('year').value;
+
+	var date = mm + "/" + dd + "/" + yyyy;
+
+	return date;
 }
 
 /*
