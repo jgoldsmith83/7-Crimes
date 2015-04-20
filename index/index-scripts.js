0b61a1e1fda61015f42effed52ddff4a829fc0a7
Justin <justingoldsmith@my.tridenttech.edu>
2015-04-19 22:32:10 -0400

    added comments to index scripts

--- index/index-scripts.js
+++ index/index-scripts.js
@@ -5,24 +5,24 @@ var currYear = new Date().getFullYear();
 
 
 function validate_age() {
-	var age_year = birthDate();
+	var age_year = birthDate(); // assigns the return value of birthDate() to a variable
 	var year = document.getElementById('year').value;
 	
 	
-	if (year < 1900 || year > currYear) {
+	if (year < 1900 || year > currYear) { // || is a logical operator meaning OR - either one side or the other must be true to run the block
 		alert("Please enter a valid birthdate");
 	} else if  (age_year < 18) {
 		alert("Sorry, we can't let you in if you're under 18. Thanks for understanding!");
-		//underAge();
+		redirectUnderAge();
 	} else {
 		alert("You're good to go!");
-		//legalAge();	
+		redirectLegalAge();	
 	}
 	
 }
 
 var birthDate = function() {
-	var num = /\d+/;
+	var num = /\d+/; // \d+ is a regular expression which finds all numbers in a string
 
 	var mm = document.getElementById('month').value;
 	var dd = document.getElementById('day').value;
@@ -31,18 +31,18 @@ var birthDate = function() {
 	var date = yyyy + mm + dd;
 
 	var dateCheck = moment(date, 'YYYYMMDD').fromNow();
-	var dateSplit = dateCheck.match(num);
-	var dateNum = parseInt(dateSplit[0]);
+	var dateSplit = dateCheck.match(num); // match() takes in a reg exp, finds all matches in a string (in this case numbers) and returns an array of results
+	var dateNum = parseInt(dateSplit[0]); // parseInt converts string input to a number - dateSplit[0] refers to the first string in the array dateSplit
 
-	return dateNum
+	return dateNum; // returns the number converted from the string at dateSplit[0]
 }
 
-var legalAge = function() {
-	window.location.href = main_url;
+var redirectLegalAge = function() {
+	window.location.href = main_url; // reloads the browser window at the url specified
 }
 
-var underAge = function() {
-	window.location.href = under_url;
+var redirectUnderAge = function() {
+	window.location.href = under_url; // reloads the browser window at the url specified
 }
 
 
